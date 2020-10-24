8d4cfa9ec4d82bb4bfd28f54fbe40bc350184054
尚格格 <44825572+shanggeg@users.noreply.github.com>
2020-10-24 17:14:25 +0800

    Add files via upload

---
+++ build/precache-manifest.daf74c5f2b8e5663ac34f195268be6d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbfac507bd47157a9418c16802e22bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54da66a9e61ced98a827",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "a39bd7356615c98dd0b2",
+    "url": "./static/css/main.389cb826.chunk.css"
+  },
+  {
+    "revision": "54da66a9e61ced98a827",
+    "url": "./static/js/2.db0f85bf.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.db0f85bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39bd7356615c98dd0b2",
+    "url": "./static/js/main.375c2587.chunk.js"
+  },
+  {
+    "revision": "705f434324a29d64b932",
+    "url": "./static/js/runtime-main.075efe5b.js"
+  },
+  {
+    "revision": "0da985759b778b8ca766e56ef2112cc2",
+    "url": "./static/media/guo.0da98575.jpg"
+  }
+]);
